9d0fabcba06d8f421dcd28c607bc7972ea97ae4c
Shashank sharma <78837537+theshashankk@users.noreply.github.com>
2021-04-29 10:42:40 +0530

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -46,11 +46,15 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 4:
-            text += f"Title - {results[i]['title']}\n"
-            text += f"Duration - {results[i]['duration']}\n"
-            text += f"Views - {results[i]['views']}\n"
-            text += f"Channel - {results[i]['channel']}\n"
-            text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
+            
+            text += f"    ╭───────•◈•───────╮
+            text += f"➼ Title - {results[i]['title']}\n"
+            text += f"➼ Duration - {results[i]['duration']}\n"
+            text += f"➼ Views - {results[i]['views']}\n"
+            text += f"➼ Channel - {results[i]['channel']}\n"
+            text += f"➼ https://youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"    ╰───────•◈•───────╯
+            text += f"    𝘗𝘰𝘸𝘦𝘳𝘦𝘥 𝘉𝘺 𝘊𝘰𝘧𝘧𝘪𝘯𝘟𝘮𝘶𝘴𝘪𝘤❤️
             i += 1
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
