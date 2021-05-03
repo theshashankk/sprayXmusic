c280b1da6a565dbd1aac5c01921b2f285d66e76a
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-03 10:49:05 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -136,7 +136,7 @@ async def playlist(client, message):
         temp.append(t)
     now_playing = temp[0][0]
     by = temp[0][1].mention(style='md')
-    msg = "**Now Playing** in {} 👻 via CoffinXmusic".format(message.chat.title)
+    msg = "**Now Playing** in {} 👻 via VcBot".format(message.chat.title)
     msg += "\n- "+ now_playing
     msg += "\n- Req by "+by
     temp.pop(0)
@@ -242,7 +242,7 @@ async def p_cb(b, cb):
             temp.append(t)
         now_playing = temp[0][0]
         by = temp[0][1].mention(style='md')
-        msg = "**Now Playing** in {} via coffinXmusic".format(cb.message.chat.title)
+        msg = "**Now Playing** in {} via VcBot".format(cb.message.chat.title)
         msg += "\n- "+ now_playing
         msg += "\n- Req by "+by
         temp.pop(0)
@@ -302,7 +302,7 @@ async def m_cb(b, cb):
             temp.append(t)
         now_playing = temp[0][0]
         by = temp[0][1].mention(style='md')
-        msg = "**Now Playing** in {} via CoffinXmusic".format(cb.message.chat.title)
+        msg = "**Now Playing** in {} via VcBot".format(cb.message.chat.title)
         msg += "\n- "+ now_playing
         msg += "\n- Req by "+by
         temp.pop(0)
@@ -406,7 +406,7 @@ async def play(_, message: Message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name =  "@CoffinXPlayer"
+        user.first_name =  "VcBot"
     usar = user
     wew = usar.id
     try:
@@ -427,7 +427,7 @@ async def play(_, message: Message):
                               await USER.join_chat(invitelink)
                               await USER.send_message(message.chat.id,"I joined this group for playing music in VC")
                               await lel.edit(
-                                  "<b>@CoffinXPlayer userbot joined your chat</b>",
+                                  "<b>Userbot joined your chat</b>",
                               )
 
                           except UserAlreadyParticipant:
@@ -436,7 +436,7 @@ async def play(_, message: Message):
                               #print(e)
                               await lel.edit(
                                   f"<b>🔴 Flood Wait Error 🔴 \nUser {user.first_name} couldn't join your group due to heavy requests for userbot! Make sure user is not banned in group."
-                                  "\n\nOr manually add @CoffinXPlayer to your Group and try again</b>",
+                                  "\n\nOr manually add to your Group and try again</b>",
                               )
                               pass
     try:
@@ -444,7 +444,7 @@ async def play(_, message: Message):
         #lmoa = await client.get_chat_member(chid,wew)
     except:
         await lel.edit(
-            f"<i> @CoffinXPlayer Userbot not in this chat, Ask admin to send /play command for first time or add assistant manually</i>"
+            f"<i> Userbot not in this chat, Ask admin to send /play command for first time or add assistant manually</i>"
         )
         return     
     sender_id = message.from_user.id
@@ -533,7 +533,7 @@ async def play(_, message: Message):
         await message.reply_photo(
         photo="final.png",
         reply_markup=keyboard,
-        caption= "Now Playing** in {} 👻 via CoffinXmusic".format(            
+        caption= "Now Playing** in {} 👻 via Vcbot".format(            
         message.from_user.mention()
         ),
     )
@@ -554,7 +554,7 @@ async def deezer(client: Client, message_: Message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name =  "coffinXmusic"
+        user.first_name =  "VcBot"
     usar = user
     wew = usar.id
     try:
