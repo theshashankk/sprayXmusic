0c2c4f7ec005d3d6363f9182351c369c3981c004
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-04 18:23:45 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -123,4 +123,4 @@ async def skip(_, message: Message):
 @errors
 async def admincache(client, message: Message):
     set(message.chat.id, [member.user for member in await message.chat.get_members(filter="administrators")])
-    #await message.reply_text("✯CoffinXmusic✯=❇️ Admin cache refreshed!")
+    #await message.reply_text("✯KHUFIYAN VC BOT✯=❇️ Admin cache refreshed!")
